f1bbb0e9b73af736bcf03cf012effd635ac77d40
hosnyaroui <hosny.cris@gmail.com>
2023-05-02 20:35:46 +0100

    first commit

---
+++ landing/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Navbar } from './components'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+<Navbar />
+
+   </div>
+
+  )
+}
+
+export default App
